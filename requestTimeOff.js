0adb118ee083011af2ee55558a6a32927baec8dd
Jason Arora <jasonarora@Jasons-MacBook-Pro.local>
2019-10-31 12:40:46 -0500

    committin

--- requestTimeOff.js
+++ requestTimeOff.js
@@ -9,13 +9,27 @@ function saveIssue (e) {
     var submitted = document.getElementById('submitted');
     var x = 1;
 
-    sendData(requestStart, requestEnd, requestDesc);
+    var sendData = checkDate(requestStart) && checkDate(requestEnd);
 
-    submitted.innerHTML = '<div class="alert alert-primary" role="alert"> Submitted </div>';
-    if (x==1) {
-        setInterval(function(){submitted.innerHTML = '<div class="alert alert-primary" role="alert"></div>';}, 3000);
-        x = 0;
+    if (sendData == true) {
+        sendData(requestStart, requestEnd, requestDesc);
+
+        submitted.innerHTML = '<div class="alert alert-primary" role="alert"> Submitted </div>';
+
+        if (x==1) {
+            setInterval(function(){submitted.innerHTML = '<div class="alert alert-primary" role="alert"></div>';}, 2000);
+            x = 0;
+        }
     }
+    else {
+        submitted.innerHTML = '<div class="alert alert-primary" role="alert"> Input Not Valid </div>';
+
+        if (x==1) {
+            setInterval(function(){submitted.innerHTML = '<div class="alert alert-primary" role="alert"></div>';}, 2000);
+            x = 0;
+        }
+    }
+
     document.getElementById('requestTimeOffForm').reset();
 
     requestTimeOff();
@@ -29,8 +43,40 @@ function sendData() {
     var reqS = arguments[0];
     var reqE = arguments[1];
     var reqD = arguments[2];
-    
-    
+
+    //ToDo: send data to firebase
+}
+
+function checkDate(date) {
+
+    var dates = date.split("/");
+
+    var month;
+    var day;
+    var year;
+
+    if (dates.length != 3){
+        return false;
+    }
+    try {
+        month = parseInt(dates[0], 10);
+        day = parseInt(dates[1], 10);
+        year = parseInt(dates[2], 10);
+    }
+    catch (err) {
+        return false;
+    }
+    if (month > 12 || month < 1) {
+        return false;
+    }
+    if (day > 31 || day < 1) {
+        return false;
+    }
+    if (year > 2100 || year < 2019) {
+        return false;
+    }
+
+    return true;
 
 }
   
