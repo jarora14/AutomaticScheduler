74bcf84f2cc32511556d897da5221c5d9409b7eb
Jason Arora <jasonarora@Jasons-MacBook-Pro.local>
2019-10-30 19:16:42 -0500

    committin

--- requestTimeOff.js
+++ requestTimeOff.js
@@ -1,16 +1,20 @@
 function requestTimeOff () {
-    document.getElementById('requestTimeOffForm').addEventListener('submit', saveIssue);
+    document.getElementById('requestTimeOffForm').addEventListener('submit', saveIssue());
   }
 
 function saveIssue (e) {
-    var requestStart = document.getElementById(requestStartInput).value;
-    var requestEnd = document.getElementById(requestEndInput).value;
-    var requestDesc = document.getElementById(requestDescInput).value;
+    var requestStart = document.getElementById('requestStartInput').value;
+    var requestEnd = document.getElementById('requestEndInput').value;
+    var requestDesc = document.getElementById('requestDescInput').value;
+
+    //need to send these^^ as a requestOff object to firebase to store which then can be accessed
+    //by admin page to view requests off
 
     document.getElementById('requestTimeOffForm').reset();
 
     requestTimeOff();
 
     e.preventDefault();
+
 }
   
