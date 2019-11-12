4134a72ac6dd928e7122dffa5c937ba1ee3463fd
Jacob Hulten <jacobhulten@Jacobs-MacBook-Pro-4.local>
2019-11-11 23:21:31 -0600

    missed file

--- public/index.js
+++ public/index.js
@@ -23,38 +23,70 @@ function validateLogin(){
     var username = document.getElementById("emailLogin").value;
     var password = document.getElementById("passwordLogin").value;
     var form = document.getElementById("form1")
-    if ( username == "test1" && password == "test1"){
-        //alert ("Login successfully");
-        window.location.href = "homeEmpl.html"; // Redirecting to other page.
-        return true;
-    }
-    else{
-        attemptLogin --;// Decrementing by one.
-        alert("You have left "+attemptLogin+" attempt;");
-        // Disabling fields after 3 attempts.
-        if( attemptLogin == 0){
-            document.getElementById("emailLogin").disabled = true;
-            document.getElementById("passwordLogin").disabled = true;
-            document.getElementById("submit").disabled = true;
-        return false;
-        }
-    }
+    verifyUserCredentials(username, password, function(bool){
+      if (bool){
+          alert("Login successful");
+          window.location.href = "homeEmpl.html"; // Redirecting to other page.
+          return true;
+      }
+      else{
+          attemptLogin --;// Decrementing by one.
+          alert("You have left "+attemptLogin+" attempt;");
+          // Disabling fields after 3 attempts.
+          if( attemptLogin == 0){
+              document.getElementById("emailLogin").disabled = true;
+              document.getElementById("passwordLogin").disabled = true;
+              document.getElementById("submit").disabled = true;
+          return false;
+          }
+      }
+    });
 }
+
 function writeUserData(userEmail, Password) {
     var database = firebase.database();
-
     var usersRef = database.ref('Users');
-
     var newUser = usersRef.push({
         user_email: userEmail,
         user_password: Password,
     });
-
     var key = newUser.key;//unique key
     console.log(key);
+}
 
-  //var userId = newUserRef.key();//unique generated id
+function checkUserExists(userEmail, Password, callback){
+    var database = firebase.database();
+    var usersRef = database.ref('Users');
+    var exists = false;
+    usersRef.once("value", function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            var childData = childSnapshot.val();
+            var childEmail = childData.user_email;
+            if (childEmail == userEmail) {
+              exists = true;
+            }
+        });
+        console.log(exists);
+        callback(exists);
+    });
+}
 
+function verifyUserCredentials(userEmail, Password, callback){
+    var database = firebase.database();
+    var usersRef = database.ref('Users');
+    var exists = false;
+    usersRef.once("value", function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            var childData = childSnapshot.val();
+            var childEmail = childData.user_email;
+            var childPassword = childData.user_password;
+            if (childEmail == userEmail && childPassword == Password) {
+              exists = true;
+            }
+        });
+        console.log(exists);
+        callback(exists);
+    });
 }
 
 function validateAdmin(){
@@ -73,24 +105,27 @@ function validateRegister(){
     var email = document.getElementById("emailRegister").value;
     var password = document.getElementById("passwordRegister").value;
     var password2 = document.getElementById("passwordRegister2").value;
-
-    if ( password2 != password){
-        alert ("Passwords do not match.");
-        return false;
-    }
-    else if (!emails.includes(email)){
-        alert("Email not verified.")
-        return false;
-    }
-    else if (registered.includes(email)){
-        alert("Email already registered.")
-        return false;
-    }
-    else{
-        writeUserData(email,password);
-        alert("Account Created!")
-        window.location.href = "index.html"; // Redirecting to other page.
-        return true;
-
-    }
+    checkUserExists(email,password, function(bool){
+        if ( password2 != password){
+            alert ("Passwords do not match.");
+            return false;
+        }
+        //Ignore Email Authentication for now. Will add firebase auth
+        /*
+        else if (!emails.includes(email)){
+            alert("Email not verified.")
+            return false;
+        }
+        */
+        else if (bool){
+            alert("Email already registered.")
+            return false;
+        }
+        else{
+            writeUserData(email,password);
+            alert("Account Created!")
+            window.location.href = "index.html";
+            return true;
+        }
+    });
 }
