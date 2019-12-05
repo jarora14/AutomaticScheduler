9f3e3cf9f7bf7b34a41bdf15d2ae888826902c97
Jason Arora <jarora@wisc.edu>
2019-12-05 16:23:48 -0600

    committin

--- schedulerLogic.js
+++ schedulerLogic.js
@@ -1,32 +1,30 @@
-//just something to get us started. Don't think this will work
+function makeSchedule() {
 
-//userEmail = document.getElementById("userEmail").value;
-var firebaseConfig = {
-    apiKey: "AIzaSyADFcVF0FfSzIyZjCL0T8Wf1jH9NA_tPqM",
-    authDomain: "realtimedatabase-d0a3e.firebaseapp.com",
-    databaseURL: "https://realtimedatabase-d0a3e.firebaseio.com",
-    projectId: "realtimedatabase-d0a3e",
-    storageBucket: "realtimedatabase-d0a3e.appspot.com",
-    messagingSenderId: "74578194003",
-    appId: "1:74578194003:web:302a3001f78fd4ee84a0a6",
-    measurementId: "G-QVGMEM66ZC"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-
-function schedule(db, userEmail) {
-
-    var database = db;
+    var database = firebase.database();
     var usersRef = database.ref('Users');
     var eventsRef = database.ref('Events');
     var scheduleRef = database.ref('Schedule');
     
-    var userData = getUserData(userEmail, database);
+    var users = [];
+    var i = 0;
+    usersRef.once("value", function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            users[i] = childSnapshot.val();
+            i++;
+        })
+    })
+    
+    var events = [];
+    var j = 0;
+    eventsRef.once("value", function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            events[i] = childSnapshot.val();
+            i++;
+        })
+    })
 
-    var updateSchedule = scheduleRef.push({
-        schedule_event: userData,
+    scheduleRef.push({
+        //push days and events
     });
     
     
@@ -45,4 +43,3 @@ function getUserData(userEmail, db){
     return childData;
 }
 
-module.exports=getUserData;
