4540b2eb9f932a3b76094126f6f8e78d228a6910
JacobHulten05 <hulten@wisc.edu>
2019-12-10 16:16:04 -0600

    testing

--- employeeStatus.test.js
+++
@@ -1,6 +0,0 @@
-const employeeStatus = require("./employeeStatus");
-
-test('Tiger Woods should be in employees', () => {
-    employees = ['Pele' ,'Michael Philips', 'Tiger Woods'];
-    expect(employees).toContain('Tiger Woods');
-});
--- setAvail.test.js
+++
@@ -1,9 +0,0 @@
-const setAvail = require('./setAvail');
-
-test('availability of employee, expecting true to be returned', () => {
-    expect(setAvail()).toBe(true);
-});
-
-test('Should be null', () => {
-    expect(setAvail().isNull()).toBeNull();
-    });
